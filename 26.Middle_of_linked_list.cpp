d817f097efc8bb1a539f6ce062ced4e34572c18e
Abhinay Challa <abhinayc365@gmail.com>
2023-06-06 10:11:16 +0530

    Create 26.Middle_of_linked_list.cpp
    Q 26

---
+++ 26.Middle_of_linked_list.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *findMiddle(Node *head) {
+    int count = 0;
+    Node* temp = head;
+    while(temp != NULL)    {
+        count++;
+        temp = temp->next;
+    }
+    int ans = count/2;
+    temp = head;
+    for(int i = 0; i < ans; i++)    {
+        temp = temp->next;
+    }
+    return temp;
+}
+
